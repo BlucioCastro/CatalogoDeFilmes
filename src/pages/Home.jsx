548e1f2f7ba0fa215a05177430e81dd5b53121da
Blucio Castro <133692344+BlucioCastro@users.noreply.github.com>
2025-11-22 14:17:32 -0300

    tradução do titulo para ingles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,19 @@ import Carousel from "../components/Carousel";
 import Footer from "../components/Footer";
 import Poster from "../components/Poster";
 
-
 export default function Home() {
 	return (
 		<div>
-			<div> 
-						<Poster type="movie" category="top_rated" />
-				<Carousel title="Séries de TV populares" url="/tv/popular" pages={3}/>
-				<Carousel title="Populares" url="/movie/popular" />
-				<Carousel title="Melhores avaliados" url="/movie/top_rated" />
-				<Carousel title="Séries melhores avaliadas" url="/tv/top_rated" />
-				<Carousel title="Em cartaz nos cinemas" url="/movie/now_playing" />
-				<Carousel title="Atualmente exibidas" url="/tv/airing_today" />
-				<Carousel title="No ar agora" url="/tv/on_the_air" />
-				<Carousel title="Próximos lançamentos" url="/movie/upcoming" />
+			<div>
+				<Poster type="movie" category="top_rated" />
+				<Carousel title="Popular TV Shows" url="/tv/popular" pages={3} />
+				<Carousel title="Popular Movies" url="/movie/popular" />
+				<Carousel title="Top Rated Movies" url="/movie/top_rated" />
+				<Carousel title="Top Rated TV Shows" url="/tv/top_rated" />
+				<Carousel title="Now in Theaters" url="/movie/now_playing" />
+				<Carousel title="Airing Today" url="/tv/airing_today" />
+				<Carousel title="On the Air Now" url="/tv/on_the_air" />
+				<Carousel title="Upcoming Releases" url="/movie/upcoming" />
 			</div>
 			<Footer />
 		</div>
